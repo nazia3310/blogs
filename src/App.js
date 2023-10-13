bec2da9727c010a14037e0a122880dc409582523
Nazia Parween <sheikh29867@gmail.com>
2023-10-13 15:44:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Header from "./components/Header";
+import Blogs from "./components/Blogs";
+import Pageination from "./components/Pageination";
+import { useContext, useEffect } from "react";
+import { AppContext } from "./context/AppContext";
+import "./App.css";
+
+export default function App() {
+  const {fetchBlogPosts} = useContext(AppContext);
+
+  useEffect(() => {
+    fetchBlogPosts();
+  },[]);
+
+  return (
+  <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center"> 
+     <Header />
+     <Blogs />
+     <Pageination />
+  </div>
+
+  );
+}
